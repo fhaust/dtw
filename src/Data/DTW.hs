41f24e359768adcaea91e38f7ce99d40bf02c189
Florian Hofmann <fho@f12n.de>
2016-07-20 16:55:23 +0200

    removed unusable UNPACK pragma

--- src/Data/DTW.hs
+++ src/Data/DTW.hs
@@ -91,7 +91,7 @@ type Index  = (Int,Int)
 type Path   = [Index]
 type Window = Set.Set Index
 
-data Result a = Result { cost :: {-# UNPACK #-} !a, path :: Path } deriving (Show,Read,Eq)
+data Result a = Result { cost :: !a, path :: !Path } deriving (Show,Read,Eq)
 
 
 -- | this is the naive implementation of dynamic time warping
